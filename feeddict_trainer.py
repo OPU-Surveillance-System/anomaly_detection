155ff939b2cc4f9764f59471dbafc6378991a918
henriojord <henriojord@gmail.com>
2017-06-24 18:45:03 +0900

    rename contribdata_trainer by trainer, and trainer by feeddict_trainer

--- feeddict_trainer.py
+++ feeddict_trainer.py
@@ -4,34 +4,13 @@ Train a given model according to a given dataset.
 
 import os
 import tensorflow as tf
-from tensorflow.python.framework import ops
+import numpy as np
+from scipy import misc
 import argparse
-import matplotlib.pyplot as plt
+from random import shuffle
+from tqdm import tqdm
 
 import vgg16
-
-def _parse_function(example_proto):
-    """
-    Parse a given tfrecord's entry into an image and a label.
-    Inputs:
-        example_proto: A tfrecord's entry.
-    Returns:
-        image_tofloat: A tensor representing the image.
-        preproc_label: A tensor representing the label.
-    """
-
-    features = {'height': tf.FixedLenFeature((), tf.int64, default_value=0),
-                'width': tf.FixedLenFeature((), tf.int64, default_value=0),
-                'label': tf.FixedLenFeature((), tf.int64, default_value=0),
-                'image': tf.FixedLenFeature((), tf.string, default_value="")}
-    parsed_features = tf.parse_single_example(example_proto, features)
-    image = tf.decode_raw(parsed_features['image'], tf.uint8)
-    image.set_shape([224 * 224 * 3])
-    image_resized = tf.reshape(image, shape=[224, 224, 3])
-    image_tofloat = tf.cast(image_resized, tf.float32)
-    preproc_label = tf.cast(parsed_features["label"], tf.float32)
-
-    return image_tofloat, preproc_label
 
 def main(args):
     #Create output directories
@@ -41,18 +20,18 @@ def main(args):
         os.makedirs(args.exp_out + '/logs')
     if not os.path.exists(args.exp_out + '/serial'):
         os.makedirs(args.exp_out + '/serial')
-    #Create dataset
-    filenames = tf.placeholder(tf.string, shape=[None])
-    dataset = tf.contrib.data.TFRecordDataset(filenames)
-    dataset = dataset.map(_parse_function)
-    dataset = dataset.shuffle(buffer_size=5000)
-    dataset = dataset.batch(args.batch_size)
-    #Create iterator
-    iterator = dataset.make_initializable_iterator()
-    image, label = iterator.get_next()
+    #Parse dataset
+    with open(args.trainset, 'r') as f:
+        content = f.read().split('\n')[:-1]
+    train_set = [d.split('\t')[0:2] for d in content]
+    with open(args.valset, 'r') as f:
+        content = f.read().split('\n')[:-1]
+    val_set = [d.split('\t')[0:2] for d in content]
     #Instantiate session
     sess = tf.InteractiveSession()
     #Instantiate model and define operations
+    image = tf.placeholder(tf.float32, [None, 224, 224, 3])
+    label = tf.placeholder(tf.float32, [None])
     model = vgg16.VGG16(image, label, args.learning_rate, args.trainable, threshold=args.threshold, weights_file=args.vgg_weights, sess=sess)
     cross_entropy = model.get_cross_entropy()
     loss_batch = model.get_loss_batch()
@@ -77,46 +56,52 @@ def main(args):
     #Init variables
     sess.run(tf.global_variables_initializer())
     sess.run(tf.local_variables_initializer())
+    sess.graph.finalize()
 
     #Training loop
     batch_count = 0
     for epoch in range(args.epochs):
         #Training
-        training_filenames = [args.train_records]
-        sess.run(iterator.initializer, feed_dict={filenames: training_filenames})
         step = 0
-        while True:
-            try:
-                t_loss, t_accuracy, _ = sess.run([loss_batch, accuracy_batch, train])
-                if step % args.summary_step == 0:
-                    print('epoch %d, step %d (%d images), loss: %.4f, accuracy: %.4f'%(epoch, step, (step + 1) * 20, t_loss, t_accuracy))
-                    feed_dict = {pl_loss: t_loss, pl_accuracy: t_accuracy}
-                    train_str = sess.run(t_summaries, feed_dict=feed_dict)
-                    train_writer.add_summary(train_str, batch_count)
-                    train_writer.flush()
-                step += 1
-                batch_count += 1
-            except tf.errors.OutOfRangeError:
-                print('Epoch %d complete'%(epoch))
-                break
+        set_size = len(train_set)
+        max_step = int(set_size / args.batch_size)
+        shuffle(train_set)
+        while step < max_step:
+            idx_start = step * args.batch_size
+            idx_end = idx_start + args.batch_size
+            x_batch = np.array([misc.imread(os.path.join('data', i[0])) for i in train_set[idx_start:idx_end]])
+            y_batch = np.array([float(l[1]) for l in train_set[idx_start:idx_end]])
+            feed_dict = {image: x_batch, label: y_batch}
+            t_loss, t_accuracy, _ = sess.run([loss_batch, accuracy_batch, train], feed_dict=feed_dict)
+            if step % args.summary_step == 0:
+                print('epoch %d, step %d (%d images), loss: %.4f, accuracy: %.4f'%(epoch, step, (step + 1) * 20, t_loss, t_accuracy))
+                feed_dict = {pl_loss: t_loss, pl_accuracy: t_accuracy}
+                train_str = sess.run(t_summaries, feed_dict=feed_dict)
+                train_writer.add_summary(train_str, batch_count)
+                train_writer.flush()
+            step += 1
+            batch_count += 1
         #Save model
         if epoch % args.save_epoch == 0:
             save_path = saver.save(sess, os.path.join(args.exp_out, 'serial/model.ckpt'), global_step=epoch)
             print('Model saved in file: %s'%(save_path))
         #Validation
-        validation_filenames = [args.val_records]
-        sess.run(iterator.initializer, feed_dict={filenames: validation_filenames})
         v_loss = 0
         v_accuracy = 0
         count = 0
-        while True:
-            try:
-                tmp_xentropy, tmp_correct_prediction = sess.run([cross_entropy, correct_prediction])
-                v_loss += sum(tmp_xentropy)
-                v_accuracy += sum(tmp_correct_prediction)
-                count += len(tmp_xentropy)
-            except tf.errors.OutOfRangeError:
-                break
+        shuffle(val_set)
+        max_step = int(100 / args.batch_size) #TODO replace 100 by args.number val sample
+        step = 0
+        for step in tqdm(range(max_step)):
+            idx_start = step * args.batch_size
+            idx_end = idx_start + args.batch_size
+            x_batch = np.array([misc.imread(os.path.join('data', i[0])) for i in val_set[idx_start:idx_end]])
+            y_batch = np.array([l[1] for l in val_set[idx_start:idx_end]])
+            feed_dict = {image: x_batch, label: y_batch}
+            tmp_xentropy, tmp_correct_prediction = sess.run([cross_entropy, correct_prediction], feed_dict=feed_dict)
+            v_loss += sum(tmp_xentropy)
+            v_accuracy += sum(tmp_correct_prediction)
+            count += len(tmp_xentropy)
         v_loss /= count
         v_accuracy /= count
         print('epoch %d validation, %d validation images, loss: %.4f, accuracy: %.4f'%(epoch, count, v_loss, v_accuracy))
@@ -128,15 +113,15 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process arguments for the model\'s trainer')
-    parser.add_argument('--lr', dest='learning_rate', type=float, default=0.000001, help='Learning rate')
+    parser.add_argument('--lr', dest='learning_rate', type=float, default=0.00001, help='Learning rate')
     parser.add_argument('--ftrain', dest='trainable', type=bool, default=False, help='Full train (VGG)')
     parser.add_argument('--weights', dest='vgg_weights', type=str, default='vgg16_weights.npz', help='Path to the VGG\'s pretrained weights')
     parser.add_argument('--thr', dest='threshold', type=float, default=0.5, help='Model\'s detection threshold')
-    parser.add_argument('--trecord', dest='train_records', type=str, default='data/train.tfrecords', help='Path to trainset tfrecords')
-    parser.add_argument('--vrecord', dest='val_records', type=str, default='data/test.tfrecords', help='Path to valset tfrecords')
-    parser.add_argument('--epochs', dest='epochs', type=int, default=200, help='Number of training epochs')
+    parser.add_argument('--tset', dest='trainset', type=str, default='data/trainset_list', help='Path to trainset')
+    parser.add_argument('--vrecord', dest='valset', type=str, default='data/testset_list', help='Path to valset')
+    parser.add_argument('--epochs', dest='epochs', type=int, default=100, help='Number of training epochs')
     parser.add_argument('--sumstep', dest='summary_step', type=int, default=50, help='Number of summary steps')
-    parser.add_argument('--saveepoch', dest='save_epoch', type=int, default=10, help='Number of save epochs')
+    parser.add_argument('--saveepoch', dest='save_epoch', type=int, default=1, help='Number of save epochs')
     parser.add_argument('--bs', dest='batch_size', type=int, default=20, help='Mini batch size')
     parser.add_argument('--out', dest='exp_out', type=str, default='exp', help='Path for experiment\'s outputs')
     args = parser.parse_args()
