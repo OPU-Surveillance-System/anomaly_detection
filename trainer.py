1dbaff704f5ea3561f0ade60abb8e5476b86f796
henriojord <henriojord@eisti.eu>
2017-07-13 15:05:19 +0900

    verif content tfrecord

--- trainer.py
+++ trainer.py
@@ -133,6 +133,7 @@ def main(args):
                 element = 0
                 for elt in tupni:
                     element += 1
+                    print(tupni.shape)
                     misc.imsave('verif/%d/%d_%d.png'%(b, element, 1), elt[0])
                     misc.imsave('verif/%d/%d_%d.png'%(b, element, 2), elt[1])
                     misc.imsave('verif/%d/%d_%d.png'%(b, element, 3), elt[2])
