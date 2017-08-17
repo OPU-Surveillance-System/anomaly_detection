1ac40db22a2542858902fc6e3749a27b75bfeed9
henriojord <henriojord@eisti.eu>
2017-08-17 15:39:46 +0900

    remvoe useless print

--- pytorchtest.py
+++ pytorchtest.py
@@ -54,9 +54,7 @@ def train_model(model, criterion, optimizer, lr_scheduler, num_epochs=25):
                 inputs = np.array([misc.imread(os.path.join('data', dsets[phase][i][0] + '.png')) for i in range(idx_start, idx_end)])
                 inputs = np.transpose(inputs, (0, 3, 1, 2))
                 labels = np.array([dsets[phase][i][1] for i in range(idx_start, idx_end)])
-                print(labels.shape)
                 labels = np.reshape(labels, (40, 1))
-                print(labels.shape)
                 #convert to tensor
                 inputs, labels = torch.from_numpy(inputs).float(), torch.from_numpy(labels).float()
                 # wrap them in Variable
