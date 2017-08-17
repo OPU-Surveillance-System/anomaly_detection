988e18c1e9fd0e67586916043aedd657f8bb5c06
henriojord <henriojord@eisti.eu>
2017-08-17 15:03:14 +0900

    reshape labels

--- pytorchtest.py
+++ pytorchtest.py
@@ -69,6 +69,7 @@ def train_model(model, criterion, optimizer, lr_scheduler, num_epochs=25):
                 # forward
                 outputs = model(inputs)
                 _, preds = torch.max(outputs.data, 1)
+                print(type(outputs), type(labels))
                 loss = nn.functional.binary_cross_entropy_with_logits(outputs, labels)
                 # backward + optimize only if in training phase
                 if phase == 'train':
