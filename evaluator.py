a5b196af26520175482f2416e3415695fc511280
henriojord <henriojord@eisti.eu>
2017-08-25 14:56:59 +0900

    add test script

--- evaluator.py
+++ evaluator.py
@@ -70,7 +70,11 @@ def main(args):
     answer, groundtruth = test_model(model)
     answer = np.array(answer)
     groundtruth = np.array(groundtruth).reshape(len(groundtruth), 1)
-    print(answer.shape, groundtruth.shape)
+    typeg=[]
+    for g in groundtruth:
+        if g not in typeg:
+            typeg.append(g)
+    print(typeg)
     fpr, tpr, thresholds = metrics.roc_curve(groundtruth, answer, pos_label=2)
     auc = metrics.auc(fpr, tpr)
     plt.figure()
