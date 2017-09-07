9dea1154ee3c8d30f63f08e1b3379f01d725418e
henriojord <henriojord@eisti.eu>
2017-09-07 15:28:52 +0900

    add vgg16lstm model and trainer

---
+++ lstm_trainer.py
@@ -0,0 +1,123 @@
+from __future__ import print_function, division
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import time
+import copy
+import os
+from scipy import misc
+import argparse
+from random import shuffle
+from importlib import import_module
+
+import plot as plt
+import data_augmentation as da
+
+def train_model(model, loss_function, optimizer):
+    """
+    """
+
+    with open(args.trainset, 'r') as f:
+        trainset = f.read().split('\n')[:-1]
+    #TODO: Sequence trainset
+    trainset = [([t.split('\t')[0] for i in range(10)], [t.split('\t')[1] for i in range(10)]) for t in trainset]
+    with open(args.valset, 'r') as f:
+        valset = f.read().split('\n')[:-1]
+    #TODO: Sequence trainset
+    valset = [([v.split('\t')[0] for i in range(10)], [v.split('\t')[1] for i in range(10)]) for v in valset]
+    dsets = {'training': trainset, 'validation': valset}
+    phase = list(dsets.keys())
+    dset_sizes = {p: len(dsets[p]) for p in phase}
+    trainepoch = 0
+    accumulated_patience = 0
+    best_loss = float('inf')
+    best_loss_acc = 0
+    best_model = copy.deepcopy(model)
+    best_trainepoch = 0
+    t_start = time.time()
+    while accumulated_patience < args.max_patience:
+        for p in phase:
+            if p == 'training':
+                model.train(True)
+            else:
+                model.train(False)
+            running_loss = 0
+            running_corrects = 0
+            shuffle(dsets[p])
+            for step in range(dset_sizes[p]):
+                #Initialize model's gradient and LSTM state
+                model.zero_grad()
+                model.hidden = model.init_hidden()
+                #Fetch sequence frames and labels
+                inputs = np.array([misc.imread(os.path.join('data', dsets[p][step][0][i] + '.png')) for i in range(10)])
+                if p == 'training' and args.augdata == 1:
+                    #TODO: Apply the same augmentation to each element
+                    inputs = da.augment_batch(inputs)
+                labels = np.array([dsets[p][step][1][i] for i in range(10)]).reshape((10, 1))
+                #Convert to cuda tensor
+                inputs, labels = torch.from_numpy(inputs).float(), torch.from_numpy(labels).float()
+                inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+                #Forward
+                logits = model(inputs)
+                probs = models.predict(logits)
+                loss = loss_function(logits, labels)
+                if p == 'training': #Backpropagation
+                    loss.backward()
+                    optimizer.step()
+                running_loss += loss.data[0]
+                running_corrects += torch.sum(probs == labels.data.long())
+            epoch_loss = running_loss / dset_size[p]
+            epoch_acc = running_corrects / dset_size[p]
+            if p == 'validation':
+                if epoch_loss < best_loss:
+                    accumulated_patience = 0
+                    best_model = copy.deepcopy(model)
+                    best_trainepoch = trainepoch
+                    best_loss = epoch_loss
+                    best_loss_acc = epoch_acc]
+                else:
+                    accumulated_patience += 1
+        trainepoch += 1
+
+    return best_model, best_trainepoch
+
+def main(args, margs):
+    """
+    1. Create a directory for the experiment results.
+    2. Dynamically instantiate the specified model and train it.
+    3. Save the trained model in the created directory.
+    """
+
+    #1.
+    if not os.path.exists(args.directory):
+        os.makedirs(args.directory)
+    #2.
+    model_import = import_module('.'.join(args.model.split('.')[0:2]))
+    model_class = getattr(model_import, args.model.split('.')[2])
+    model = model_class(margs)
+    #model.cuda()
+    loss_function = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.trainable_parameters, args.learning_rate)
+    trained_model, best_trainepoch = train_model(model, loss_function, optimizer)
+    #3.
+    #torch.save(trained_model, os.path.join(args.directory, 'trained_model'))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--m', dest='model', type=str, default='models.vgg16lstm.VGG16LSTM', help='')
+    parser.add_argument('--bs', dest='batch_size', type=int, default=1, help='')
+    parser.add_argument('--lr', dest='learning_rate', type=float, default=0.001, help='')
+    parser.add_argument('--p', dest='max_patience', type=int, default=10, help='')
+    parser.add_argument('--tr', dest='trainset', type=str, default='data/trainset_labels', help='')
+    parser.add_argument('--val', dest='valset', type=str, default='data/valset_labels', help='')
+    parser.add_argument('--dir', dest='directory', type=str, default='experiment', help='')
+    parser.add_argument('--da', dest='augdata', type=int, default=1, help='')
+    parser.add_argument('--plot', dest='plot', type=int, default=1, help='')
+    args, unknown = parser.parse_known_args()
+    margs = {u.split('=')[0][2:]:u.split('=')[1] for u in unknown}
+    print('arguments passed to the model: {}'.format(margs))
+    main(args, margs)
---
+++ models/vgg16lstm.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+import torchvision
+from torchvision import datasets, models, transforms
+
+class VGG16LSTM(nn.Module):
+    """
+    """
+
+    def __init__(self, margs):
+        """
+        --bn: Batch Norm (bool)
+        --do: Dropout prob (float)
+        --wl: Weighted loss (bool)
+        --lr: Learning rate (float)
+        --thr: Detection threshold (float)
+        --hs: LSTM Hidden state's size
+        --rl: LSTM's number of layers
+        """
+
+        if 'bn' not in margs.keys(): #Batch norm
+            margs['bn'] = False
+        if 'do' not in margs.keys(): #Dropout
+            margs['do'] = 0.0
+        if 'wl' not in margs.keys(): #weighted loss
+            margs['wl'] = False
+        if 'thr' not in margs.keys(): #Detection threshold
+            margs['thr'] = 0.5
+        if 'hd' not in margs.keys(): #LSTM Hidden state's dimension
+            margs['hd'] = 100
+        if 'rl' not in margs.keys(): #LSTM's number of layers
+            margs['rl'] = 1
+
+        super(VGG16LSTM, self).__init__()
+        self.margs = margs
+        #VGG part
+        self.vgg = models.vgg16(pretrained=True) #Load pretrained VGG16
+        mod = list(self.vgg.classifier.children()) #Remove the final layer
+        mod.pop()
+        new_classifier = torch.nn.Sequential(*mod)
+        self.vgg.classifier = new_classifier
+        for param in self.vgg.parameters(): #Freeze convolutional layers
+            param.requires_grad = False
+        #LSTM part
+        self.rnn = nn.LSTM(input_size=4096,
+                           hidden_size=self.margs['hd'],
+                           num_layers=self.margs['rl'],
+                           dropout=self.margs['do'])
+        self.hidden = self.init_hidden()
+        self.trainable_parameters = list(self.rnn.parameters())
+        self.out_layer = nn.Linear(self.margs['hd'], 1)
+        self.trainable_parameters += list(self.out_layer.parameters())
+
+    def init_hidden(self):
+        """
+        """
+
+        #Dimension semantic: Number of recurent layers, Batch size, Hidden size
+        init = (Variable(torch.zeros(self.margs['rl'], 1, self.margs['hd'])),
+                Variable(torch.zeros(self.margs['rl'], 1, self.margs['hd'])))
+
+        return init
+
+    def forward(self, frames):
+        """
+        input dimensions: [Sequence, Height, Width, Channels]
+        """
+
+        frames = frames.transpose((0, 3, 1, 2))
+        embeds = self.vgg(frames)
+        lstm_out, self.hidden = self.lstm(embeds.view(len(frames), 1, -1), self.hidden)
+        logits = self.out_layer(lstm_out.view(len(frames), -1))
+
+        return logits
+
+    def predict(self, probs):
+        """
+        """
+
+        return (torch.sigmoid(probs.data) > float(self.margs['thr'])).long()
