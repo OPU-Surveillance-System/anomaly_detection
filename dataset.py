95f0516611570f6444b48a384296b2f5e4d72195
henriojord <henriojord@eisti.eu>
2017-09-12 16:13:53 +0900

    add dataset

--- dataset.py
+++ dataset.py
@@ -33,9 +33,9 @@ class MiniDroneVideoDataset(Dataset):
                 video = c[0].split('/')[-1].split('-')[0]
                 tmp = [c]
         groups.append(tmp)
-        print(groups[0][0])
+        print((groups[0][0][0:self.sequence_length], groups[0][0][self.sequence_length:]))
         #Build sequences from the same video of length sequence_length
-        self.frames = [(['data/' + f[0] + '.png' for f in g[i:i+self.sequence_length]], [int(f[1]) for f in g[i:i+self.sequence_length]])
+        self.frames = [([os.path.join(self.root_dir, f[0] + 'png') for f in g[i][0:self.sequence_length]], [int(f[1]) for f in g[i][self.sequence_length:]])
                        for g in groups for i in range(len(g) - (self.sequence_length - 1))]
 
     def __len__(self):
