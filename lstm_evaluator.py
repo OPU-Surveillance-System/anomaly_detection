8fbeeb21a5f88264e4ca092c3603c60aec36ac55
henriojord <henriojord@eisti.eu>
2017-09-13 13:13:26 +0900

    remove useless scripts

--- lstm_evaluator.py
+++ lstm_evaluator.py
@@ -58,17 +58,8 @@ def main(args):
     print(set(answer))
     fpr, tpr, thresholds = metrics.roc_curve(groundtruth, answer)
     auc = metrics.auc(fpr, tpr)
-    plt.figure()
-    lw = 2
-    plt.plot(fpr, tpr, color='darkorange', lw=lw, label='ROC curve (area = %0.2f)' % auc)
-    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic')
-    plt.legend(loc="lower right")
-    plt.savefig(os.path.join(args.directory, os.path.basename(args.model) + '_roc.svg'), format='svg')
+    plot.plot_auc(auc, fpr, tpr, args.directory, os.path.basename(args.model))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process arguments for the model\'s trainer')
--- plot.py
+++ plot.py
@@ -10,3 +10,16 @@ def plot_history(history, directory):
     axes[0].legend(loc='lower right')
     axes[1].legend(loc='lower right')
     plt.savefig(os.path.join(directory, 'training.svg'), format='svg')
+
+def plot_auc(auc, fpr, tpr, directory, name):
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange', lw=lw, label='ROC curve (area = %0.2f)' % auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.savefig(os.path.join(directory, name + '_roc.svg'), format='svg')
