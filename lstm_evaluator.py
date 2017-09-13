2d4e04e6729fe8dda26616b99dbea9fe2671542b
henriojord <henriojord@eisti.eu>
2017-09-13 12:12:05 +0900

    debug lstm eval

--- lstm_evaluator.py
+++ lstm_evaluator.py
@@ -44,7 +44,7 @@ def test_model(model):
         # for p in probs:
         #     answer.append((Variable(p).data).cpu().numpy())
         groundtruth += labels.data.cpu().numpy()
-        answer += probs.data
+        answer += Variable(probs).data.cpu().numpy()
 
     time_elapsed = time.time() - since
     print('Testing complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
