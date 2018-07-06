f6ce29ab163df64d17bd75ad067833a42993e932
henriojord <henriojord@gmail.com>
2018-07-06 16:49:15 +0900

    fix type

--- dataset.py
+++ dataset.py
@@ -119,7 +119,7 @@ class NegativeDataset(Dataset):
         else:
             dataset = self.abnormal
             label = 1
-        x = misc.imread(dataset[idx])#.reshape(224, 224, 1)
+        x = misc.imread(dataset[idx]).astype(np.float32)#.reshape(224, 224, 1)
         #x = (x - x.min()) / (x.max() - x.min())
         name = dataset[idx]
 
